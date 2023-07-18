6fe31b75ec5d99af033fc5e51a2e8566f201f300
Alistair Tait <alistair.tait1@gmail.com>
2023-07-18 20:18:47 +0100

    #1 GREEN: Implemented view model builder which passes tests

--- FridgeConsole/FridgeMessageViewModelBuilder.cs
+++ FridgeConsole/FridgeMessageViewModelBuilder.cs
@@ -6,6 +6,33 @@ public class FridgeMessageViewModelBuilder : IFridgeMessageViewModelBuilder
 {
     public IEnumerable<CharacterModel> BuildModel(string messageCandidate, List<CharacterInventoryModel> inventory)
     {
-        throw new NotImplementedException();
+        var characters = messageCandidate.ToCharArray().ToList();
+        var characterModels = new List<CharacterModel>();
+
+        foreach (var character in characters)
+        {
+            if (character == ' ')
+            {
+                characterModels.Add(new() { Character = ' ', IsAvailable = true });
+                continue;
+            }
+
+            if (CharacterIsAvailable(character, inventory))
+            {
+                characterModels.Add(new() { Character = character, IsAvailable = true });
+                inventory.FirstOrDefault(model => model.Character == character).QuantityAvailable--;
+                continue;
+            }
+
+            characterModels.Add(new() { Character = character, IsAvailable =  false });
+        }
+
+        return characterModels;
+    }
+
+    private bool CharacterIsAvailable(char character, List<CharacterInventoryModel> inventory)
+    {
+        return inventory.Select(model => model.Character).Contains(character) &&
+            inventory.FirstOrDefault(model => model.Character == character).QuantityAvailable > 0;
     }
 }
