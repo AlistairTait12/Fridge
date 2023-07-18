14a45d135be64716111f9696029d178b9662fcb1
Alistair <74990104+AlistairTait12@users.noreply.github.com>
2023-07-18 22:03:47 +0100

    Feature/1 availability viewmodel (#3)
    * RED: #1 Added view model builder tests
    
    * #1 GREEN: Implemented view model builder which passes tests
    
    * #1 REFACTOR: Applied refactoring and added another unit test

---
+++ FridgeConsole/FridgeMessageViewModelBuilder.cs
@@ -0,0 +1,36 @@
+﻿using FridgeConsole.Models;
+
+namespace FridgeConsole;
+
+public class FridgeMessageViewModelBuilder : IFridgeMessageViewModelBuilder
+{
+    public IEnumerable<CharacterModel> BuildModel(string messageCandidate, List<CharacterInventoryModel> inventory)
+    {
+        var characters = messageCandidate.ToCharArray().ToList();
+        var characterModels = new List<CharacterModel>();
+
+        foreach (var character in characters)
+        {
+            if (Equals(character, ' '))
+            {
+                characterModels.Add(new() { Character = ' ', IsAvailable = true });
+                continue;
+            }
+
+            if (CharacterIsAvailable(character, inventory))
+            {
+                characterModels.Add(new() { Character = character, IsAvailable = true });
+                inventory.FirstOrDefault(model => Equals(model.Character, character)).QuantityAvailable--;
+                continue;
+            }
+
+            characterModels.Add(new() { Character = character, IsAvailable =  false });
+        }
+
+        return characterModels;
+    }
+
+    private bool CharacterIsAvailable(char character, List<CharacterInventoryModel> inventory) =>
+        inventory.Select(model => model.Character).Contains(character)
+        && inventory.FirstOrDefault(model => Equals(model.Character, character)).QuantityAvailable > 0;
+}
